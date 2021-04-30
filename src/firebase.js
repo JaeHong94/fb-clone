42250b4c0af01082363c31956d4d8eaaa0b0a7ef
JaeHongMin94 <moonshin123@naver.com>
2021-05-01 05:39:55 +0900

    Create a Header and Header style

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWeNxz8Tf-QfeNuiIQCuYs54k0gWBKn30',
+  authDomain: 'facebook-clone-f97d9.firebaseapp.com',
+  projectId: 'facebook-clone-f97d9',
+  storageBucket: 'facebook-clone-f97d9.appspot.com',
+  messagingSenderId: '1038154073879',
+  appId: '1:1038154073879:web:d2feee23f8803965c21952',
+  measurementId: 'G-0DBYN64TVW',
+};
+
+export default firebaseConfig;
